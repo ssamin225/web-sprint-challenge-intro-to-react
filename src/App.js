fd2c877c3db17480451813f735420c4a9289efc0
ssamin225 <sadmansamin225@gmail.com>
2021-10-08 15:14:37 -0400

    inserted data into the DOM and styled some elements

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import Character from './components/Character';
+import styled from 'styled-components';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,10 +11,27 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people')
+      .then(res => {
+        console.log(res.data);
+        setCharacters(res.data);
+      }).catch(err => {
+        console.log(err);
+      })
+  }, [])
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {characters.length === 0
+      ? <h2>Loading...</h2>
+      :
+      characters.map(character => (
+        <Character key={character.name} character={character}/>
+      ))}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,67 @@
 // Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+const StyledCharacters = styled.div`
+    width: 50%;
+    margin: auto;
+    background-color: #b9936c;
+
+    table {
+        width: 95%;
+        margin: auto;
+        text-align: left;
+        .keys {
+            background-color: #dac292;
+        }
+        .values {
+            background-color: #e6e2d3;
+        }
+    }
+`
+
+const Character = ({ character }) => {
+    return (
+        <StyledCharacters>
+            <h2>{character.name}</h2>
+            <table>
+                <colgroup>
+                        <col span='1' className='keys'/>
+                        <col span='1' className='values'/>
+                </colgroup>
+                <tbody>
+                    <tr>
+                        <th>Birth Year</th>
+                        <td>{character.birth_year}</td>
+                    </tr>
+                    <tr>
+                        <th>Gender</th>
+                        <td>{character.gender}</td>
+                    </tr>
+                    <tr>
+                        <th>Height</th>
+                        <td>{character.height}</td>
+                    </tr>
+                    <tr>
+                        <th>Mass</th>
+                        <td>{character.mass}</td>
+                    </tr>
+                    <tr>
+                        <th>Hair Color</th>
+                        <td>{character.hair_color}</td>
+                    </tr>
+                    <tr>
+                        <th>Eye Color</th>
+                        <td>{character.eye_color}</td>
+                    </tr>
+                    <tr>
+                        <th>Skin Color</th>
+                        <td>{character.skin_color}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </StyledCharacters>
+    )
+}
+
+export default Character;
